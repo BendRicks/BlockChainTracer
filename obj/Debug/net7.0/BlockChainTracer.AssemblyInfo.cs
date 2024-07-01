1913047d6720062c87a4809f9d813ce0e59c398f
BendRicks <bendricks@yandex.ru>
2024-07-01 03:18:09 +0300

    update gitignore 2

--- obj/Debug/net7.0/BlockChainTracer.AssemblyInfo.cs
+++ obj/Debug/net7.0/BlockChainTracer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlockChainTracer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32d4cf572d0091de558c6b28cb388703dd22a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlockChainTracer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlockChainTracer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
